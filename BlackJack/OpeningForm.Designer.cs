83b992e5be51722a54f06b0d9244e3e4957bf53a
Paul Herron <pol.herron@gmail.com>
2017-04-01 12:29:20 +0100

    Added Button

--- BlackJack/OpeningForm.Designer.cs
+++ BlackJack/OpeningForm.Designer.cs
@@ -32,13 +32,15 @@
             this.txtCard = new System.Windows.Forms.TextBox();
             this.rtbOutput = new System.Windows.Forms.RichTextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Location = new System.Drawing.Point(16, 15);
+            this.btnStart.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.Size = new System.Drawing.Size(100, 28);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start Game";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -46,38 +48,52 @@
             // 
             // txtCard
             // 
-            this.txtCard.Location = new System.Drawing.Point(32, 49);
+            this.txtCard.Location = new System.Drawing.Point(43, 60);
+            this.txtCard.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtCard.Name = "txtCard";
-            this.txtCard.Size = new System.Drawing.Size(216, 20);
+            this.txtCard.Size = new System.Drawing.Size(287, 22);
             this.txtCard.TabIndex = 1;
             // 
             // rtbOutput
             // 
-            this.rtbOutput.Location = new System.Drawing.Point(32, 75);
+            this.rtbOutput.Location = new System.Drawing.Point(43, 92);
+            this.rtbOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.rtbOutput.Name = "rtbOutput";
-            this.rtbOutput.Size = new System.Drawing.Size(216, 174);
+            this.rtbOutput.Size = new System.Drawing.Size(287, 213);
             this.rtbOutput.TabIndex = 2;
             this.rtbOutput.Text = "";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(103, 11);
+            this.button1.Location = new System.Drawing.Point(137, 14);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(100, 28);
             this.button1.TabIndex = 3;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // PlayBlackjack
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(379, 321);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.rtbOutput);
             this.Controls.Add(this.txtCard);
             this.Controls.Add(this.btnStart);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "PlayBlackjack";
             this.Text = "BlackJack";
             this.ResumeLayout(false);
@@ -91,6 +107,7 @@
         private System.Windows.Forms.TextBox txtCard;
         private System.Windows.Forms.RichTextBox rtbOutput;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
